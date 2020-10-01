c4d4f113ca07aa721fa1a955ebee7f09cec784b1
Yasin Zhang <zyxuan97@foxmail.com>
2020-10-01 22:11:17 +0800

    init project

---
+++ src/main/java/com/yasin/usercenter/TestController.java
@@ -0,0 +1,31 @@
+package com.yasin.usercenter;
+
+import com.yasin.usercenter.dao.user.UserMapper;
+import com.yasin.usercenter.domain.entity.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * @author Yasin Zhang
+ */
+@RestController("/")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class TestController {
+    private final UserMapper userMapper;
+
+    @GetMapping("test")
+    public User testInsert() {
+        User user = new User();
+        user.setAvatarUrl("xxx");
+        user.setBonus(100);
+        user.setCreateTime(new Date());
+        user.setUpdateTime(new Date());
+        this.userMapper.insertSelective(user);
+
+        return user;
+    }
+}
